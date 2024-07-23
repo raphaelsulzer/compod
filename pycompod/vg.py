7c02f6038cfe9eeab0b04d5ff8cc4234ba12c9d3
Lizeth Fuentes <lztfuentespz@gmail.com>
2024-07-23 16:23:03 +0200

    add sphere example and reader for vg input file

--- pycompod/vg.py
+++ pycompod/vg.py
@@ -48,6 +48,8 @@ class VertexGroup:
         ending = os.path.splitext(self.input_file)[1]
         if ending == ".npz":
             self._process_npz()
+        elif ending == ".vg":
+            self._process_vg()
         else:
             self.logger.error("{} is not a valid file type for planes. Only .npz files are allowed.".format(ending))
             sys.exit(1)
@@ -354,6 +356,252 @@ class VertexGroup:
                 if np.dot(pl[:3],plane_normal) < 0:
                     self.planes[i] = -pl
 
+    def extract_numbers_from_line(self, line):
+        import re
+        # Define a regular expression pattern to find all numbers in the line
+        pattern = r'\d+\.?\d*'  # This will match integers and decimal numbers
+
+        # Find all matches of the pattern in the line
+        matches = re.findall(pattern, line)
+
+        # Convert matches to float or int
+        numbers = [float(match) if '.' in match else int(match) for match in matches]
+
+        return numbers
+    def _load_vg(self):
+        primitives_file = self.input_file
+        points = []
+        normals = []
+        groups = []
+        planes = []
+        colors = []
+        with open(primitives_file, 'r') as file:
+            # Read the file line by line
+            with open(primitives_file, 'r') as file:
+                # Extract numbers from the current line
+                number_line = file.readline()
+                line_numbers = self.extract_numbers_from_line(number_line)
+                n_poins = line_numbers[0]
+                # read the n_points
+                for i in range(n_poins):
+                    line = file.readline()
+                    [x, y, z] = line.split()
+                    points.append([float(x), float(y), float(z)])
+
+                number_line = file.readline()
+
+                for i in range(n_poins):
+                    line = file.readline()
+
+                number_line = file.readline()
+
+                for i in range(n_poins):
+                    line = file.readline()
+                    [x, y, z] = line.split()
+                    normals.append([float(x), float(y), float(z)])
+
+                number_line = file.readline()
+                line_numbers = self.extract_numbers_from_line(number_line)
+                n_groups = line_numbers[0]
+
+                for i in range(n_groups):
+                    number_line = file.readline() # group_type
+                    number_line = file.readline() # num_group_parameters: 4
+                    number_line = file.readline() # group_parameters: a b c d
+                    [t, a, b, c, d] = number_line.split()
+                    h_list = [float(a), float(b), float(c), float(d)]
+                    planes.append(h_list)
+                    number_line = file.readline() # group_label: unknown
+                    number_line = file.readline() # group_color: r g b
+                    [t, r, g, b] = number_line.split()
+                    color_list = [float(r), float(g), float(b)]
+                    colors.append(color_list)
+
+                    number_line = file.readline() # roup_num_point
+                    line_numbers = self.extract_numbers_from_line(number_line)
+                    line = file.readline()
+                    str_numbers = line.split()
+                    int_list = [int(num) for num in str_numbers]
+                    groups.append(int_list)
+                    number_line = file.readline()
+
+        return points, normals, groups, planes, colors
+
+    def _process_vg(self):
+        """
+                Load vertex groups and planes from npz file.
+                :return:
+                """
+        points, normals, groups, planes, colors = self._load_vg()
+
+        type = np.float64
+
+        points = np.array(points).astype(type)
+        colors = np.array(colors).astype(type)
+        planes = np.array(planes).astype(type)
+        normals = np.array(normals).astype(type)
+
+        groups = [np.array(sublist, dtype=np.int32) for sublist in groups]
+
+        # read the data and make the point groups
+        self.planes = planes
+        self.plane_colors = colors
+        self.points = points
+        self.normals = normals
+        # self.classes = data.get("classes", np.ones(len(self.points),dtype=np.int32))
+        self.classes = []
+        if not len(self.classes):
+            self.classes = None
+        self.groups = groups
+
+        self.logger.info(
+            "Loaded {} inlier points of {} planes".format(np.concatenate(self.groups).shape[0], len(self.planes)))
+
+        if self.epsilon is not None:
+            self._cluster_planes_with_abc_and_origin_dist()
+
+        ### orient planes according to the mean normal orientation of it's input points
+        ### cannot actually use this before merging, because I want to merge oppositely oriented planes!
+        ### can also not do it after merge, see below!
+        # self._orient_planes()
+
+        self.halfspaces = []
+        self.polygons = []
+        self.polygon_areas = []
+        self.projected_points = np.zeros(shape=(self.points.shape[0], 2))
+        self.hull_vertices = []
+        self.n_fill = 0
+        for i, vert_group in enumerate(self.groups):
+            self.halfspaces.append([Polyhedron(ieqs=[inequality]) for inequality in self._inequalities(self.planes[i])])
+            pts = self.points[vert_group]
+
+            # TODO: i am computing the convex hull twice below; not necessary
+            ## this is only used for sorting the polys by area
+            pl = PyPlane(self.planes[i])
+            try:
+                poly = pl.get_trimesh_of_projected_points(pts, type="convex_hull")
+                self.polygons.append(poly)
+                self.polygon_areas.append(poly.area)
+            except:
+                self.logger.warning("Degenerate input polygon.")
+                self.polygons.append(trimesh.Trimesh)
+                self.polygon_areas.append(0)
+
+            ## this is used for finding points associated to facets of the partition for normal based occupancy voting
+            self.projected_points[vert_group] = pl.to_2d(pts)
+
+            ## this is used for _get_best_plane function
+            try:
+                pch = ProjectedConvexHull(self.planes[i], pts)
+                self.hull_vertices.append(vert_group[pch.hull.vertices])
+                n_hull_vertices = len(pch.hull.vertices)
+            except:
+                self.hull_vertices.append(vert_group)
+                n_hull_vertices = len(vert_group)
+
+            if n_hull_vertices > self.n_fill:
+                self.n_fill = n_hull_vertices
+
+        assert self.points.shape[0] == self.projected_points.shape[0]
+        self.polygons = np.array(self.polygons)
+        self.polygon_areas = np.array(self.polygon_areas)
+
+        if self.points_type == "samples":
+            self.logger.info(
+                "Sample a total of {} points on {} polygons".format(self.total_sample_count, len(self.polygons)))
+            ### scale sample_count_per_area by total area of input polygons. like this n_sample_points should roughly be constant for each mesh + (convex hull points)
+            self.sample_count_per_area = self.total_sample_count / self.polygon_areas.sum()
+            self.points = []
+            self.normals = None
+            self.groups = []
+            self.hull_vertices = []
+            n_points = 0
+            for i, poly in enumerate(self.polygons):
+                n = 3 + int(self.sample_count_per_area * self.polygon_areas[i])
+                sampled_points = poly.sample(n)
+                sampled_points = np.concatenate((poly.vertices, sampled_points), axis=0, dtype=np.float64)
+                self.points.append(sampled_points)
+                self.groups.append(np.arange(len(sampled_points), dtype=np.int32) + n_points)
+                self.hull_vertices.append(np.arange(len(poly.vertices), dtype=np.int32) + n_points)
+                n_points += sampled_points.shape[0]
+            self.points = np.concatenate(self.points)
+        elif self.points_type == "inliers":
+            pass
+        else:
+            print("{} is not a valid point_type. Only 'inliers' or 'samples' are allowed.".format(self.points_type))
+            raise NotImplementedError
+
+        ## recolor planes and support points
+        if self.recolor:
+            self._recolor_planes()
+            # save with new colors
+            data = dict(data)
+            data["group_colors"] = self.plane_colors
+            np.savez(self.input_file, **data)
+
+        if self.export:
+            ## export planes and samples
+            pt_file = os.path.splitext(self.input_file)[0] + "_samples.ply"
+            plane_file = os.path.splitext(self.input_file)[0] + '.ply'
+            self.plane_exporter.save_points_and_planes(point_filename=pt_file, plane_filename=plane_file,
+                                                       points=self.points, normals=self.normals, groups=self.groups,
+                                                       planes=self.planes, colors=self.plane_colors)
+
+        if self.prioritise is not None:
+            order = self._prioritise_planes(self.prioritise)
+            self.plane_order = order
+            self.groups = np.array(self.groups, dtype=object)[order]
+            if not self.groups[
+                       0].dtype == np.int32:  # this line is only here for the rare case that all group arrays are of the same size, then numpy sets the dtype of each individual array to object
+                self.groups = list(self.groups.astype(np.int32))
+            else:
+                self.groups = list(self.groups)
+            self.planes = self.planes[order]
+            self.plane_colors = self.plane_colors[order]
+            self.halfspaces = list(np.array(self.halfspaces)[order])
+            self.hull_vertices = np.array(self.hull_vertices, dtype=object)[order]
+            if not self.hull_vertices[
+                       0].dtype == np.int32:  # this line is only here for the rare case that all hull_vertices arrays are of the same size, then numpy sets the dtype of each individual array to object
+                self.hull_vertices = self.hull_vertices.astype(np.int32)
+        else:
+            self.plane_order = np.arange(len(self.planes))
+            self.logger.info("No plane prioritisation applied")
+        del self.polygons
+        del self.polygon_areas
+
+        ## Note that we keep the prioritisation of planes when merging duplicates
+        if self.merge_duplicate_planes:
+            self.merged_plane_from_input_planes = self._merge_duplicate_planes()
+        ## even if duplicate planes are not merged, check for them, for evaluation purposes
+        else:
+            self.merged_plane_from_input_planes = []
+            for p in self.planes:
+                self.merged_plane_from_input_planes.append(np.where((p == self.planes).all(axis=1))[0])
+
+        ### orient planes according to the mean normal orientation of it's input points
+        ### leads to a crash to do this down here, most likely because hspace orientations do not correspond to planes anymore
+        # self._orient_planes()
+
+        ## export planes and samples
+        if self.export:
+            pt_file = os.path.splitext(self.input_file)[0] + "_samples_merged.ply"
+            plane_file = os.path.splitext(self.input_file)[0] + '_merged.ply'
+            self.plane_exporter.save_points_and_planes(plane_filename=plane_file, point_filename=pt_file,
+                                                       points=self.points, normals=self.normals, groups=self.groups,
+                                                       planes=self.planes, colors=self.plane_colors)
+
+        ## fill the hull_vertices array to make it a matrix instead of jagged array for an efficient _get_best_plane function with matrix multiplications
+        ## if torch.nested.nested_tensor ever supports broadcasting and dot products, the code could be simplified a lot.
+        self.n_fill = self.n_fill * 2
+        self._fill_hull_vertices()
+
+        ## this is only used for abspy construction
+        self.bounds = []
+        for group in self.groups:
+            pts = self.points[group]
+            bounds = np.array([np.amin(pts, axis=0), np.amax(pts, axis=0)])
+            self.bounds.append(bounds)
+        self.bounds = np.array(self.bounds)
     def _process_npz(self):
         """
         Load vertex groups and planes from npz file.
